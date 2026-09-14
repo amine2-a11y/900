395907ecae63470e232048647f048237b0f42e03
ps4 <elhainaamine2@gmail.com>
2026-09-14 17:50:11 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,37 @@
-const CACHE_NAME = 'ps4-goldhen-offline-v1';
+const CACHE_NAME = 'ps4-goldhen-offline-v3';
 const CORE = [
-  './',
-  './index.html',
-  './src/psfree-lapse/psfree.mjs',
-  './includes/payloads/GoldHEN/goldhen.bin'
+  "./includes/payloads/GoldHEN/goldhen.bin",
+  "./index.html",
+  "./src/psfree-lapse/alert.mjs",
+  "./src/psfree-lapse/config.mjs",
+  "./src/psfree-lapse/kpatch/900.bin",
+  "./src/psfree-lapse/lapse.mjs",
+  "./src/psfree-lapse/lapse/ps4/900.mjs",
+  "./src/psfree-lapse/module/chain.mjs",
+  "./src/psfree-lapse/module/int64.mjs",
+  "./src/psfree-lapse/module/mem.mjs",
+  "./src/psfree-lapse/module/memtools.mjs",
+  "./src/psfree-lapse/module/offset.mjs",
+  "./src/psfree-lapse/module/rw.mjs",
+  "./src/psfree-lapse/module/utils.mjs",
+  "./src/psfree-lapse/module/view.mjs",
+  "./src/psfree-lapse/psfree.mjs",
+  "./src/psfree-lapse/rop/ps4/900.mjs",
+  "./sw.js",
+  "./theme/pro-void.css"
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(CORE).catch(() => cache))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then(async cache => {
+      for (const url of CORE) {
+        try {
+          const response = await fetch(url, {cache:'no-store'});
+          if (response.ok) await cache.put(url, response.clone());
+        } catch (_) {}
+      }
+      await self.skipWaiting();
+    })
   );
 });
 
